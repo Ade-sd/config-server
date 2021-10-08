74bca628e8ace46a337d3d6381775df4632cc3de
adesd <adesd16@gmail.com>
2021-10-08 19:40:53 +0330

    config server first commit

---
+++ src/main/java/com/nars/NarsConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.nars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarsConfigServerApplication.class, args);
+	}
+
+}
